d3f3191cb13792f335b8ce05c17cf11eb9a3d9f2
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2023-07-17 15:49:30 +0000

    Add bumo effect to the art icon for when item gets added to the cart

--- hungrybelly/src/Components/Header/CartButton.js
+++ hungrybelly/src/Components/Header/CartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/context';
 import styles from '../Header/CartButton.module.css'
@@ -6,15 +6,31 @@ import styles from '../Header/CartButton.module.css'
 
 const CartButton = props => {
 
+    const [cartHighlight, setCartHighlight] = useState(false)
     const ctx = useContext(CartContext);
 
     const itemAmount = ctx.item.reduce((acc,val)=>{
         return acc+val.amount
     },0)
 
+    const btnClasses = `${styles.button} ${cartHighlight ? styles.bump:''}`
+
+    useEffect(()=>{
+
+        if(ctx.item.length===0){
+            return
+        };
+
+        setCartHighlight(true);
+        const timer = setTimeout(()=>setCartHighlight(false),300);
+        return ()=>{clearTimeout(timer)};
+
+
+    }, [ctx.item])
+
     return(
         <React.Fragment>
-            <button className={styles.button} onClick={props.onOpen}>
+            <button className={btnClasses} onClick={props.onOpen}>
                 <span className={styles.icon}><CartIcon /></span>
                 <span>Your Cart</span>
                 <span className={styles.badge}>{itemAmount}</span>
